672ee25a4674f1ecddd85ab368ec6fc2af0c27e9
purva0401 <purvachavrekar2002@gmail.com>
2024-01-01 20:20:51 +0530

    To create contacts

---
+++ src/addressbook/AddressBook.java
@@ -0,0 +1,13 @@
+package addressbook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+}
+
---
+++ src/addressbook/AdressBookMain.java
@@ -0,0 +1,14 @@
+package addressbook;
+
+public class AdressBookMain {
+    public static void main(String[] args) {
+
+        Contact c1 = new Contact("Purva", "Chavrekar", "Near Sai Mandir", "Roha", "Maharastra", 402019, "7164883247", "purvachavrekar@gmail.com");
+        Contact c2 = new Contact("Omkar", "Madhavi", "Jubilee Hills", "Panvel", "Maharastra", 410340, "7845236291", "omkaemadhvi@gmail.com");
+        Contact c3 = new Contact("Pranjal", "Khade", "Visava Villa", "Banglore", "Karnataka", 493123, "9590595867", "pranjalkhade@gmail.com");
+        AddressBook a1 = new AddressBook();
+
+
+
+    }
+}
